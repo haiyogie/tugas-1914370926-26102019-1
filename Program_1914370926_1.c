97348f1d77f24ee658cda8fe4e5783fd45796234
YOGIE HIDAYAT <55613630+haiyogie@users.noreply.github.com>
2019-10-31 23:49:53 +0700

    Add files via upload

---
+++ Program_1914370926_1.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+int main()
+{
+    char NPM[30];
+    char Nama[30];
+    char Status_kelulusan;
+    char hasil;
+    int Nilai_UTS,Nilai_UAS,cek;
+    float Nilai_Akhir;	
+printf ("\nNama:Yogie Hidayat");
+printf ("\nNpm : 1914370926");
+Nilai_UTS = 80 ;
+Nilai_UAS = 75 ;
+printf ("\nNilai UTS \t: %i",Nilai_UTS);
+printf ("\nNilai UAS \t: %i",Nilai_UAS);
+    if (Nilai_UTS > 70);
+hasil = "LULUS."; 
+printf ("\nNilai Akhir\t: %i",Nilai_UTS);
+if (Nilai_UTS <= 70);
+    hasil = "LULUS." ;
+if (Nilai_UTS >= 60); 
+hasil = "Nilai_Akhir = (Nilai_UTS*0,4)+(Nilai_UAS*0,6)";
+printf  ("\nNilai Akhir\t: %.2f", Nilai_Akhir);
+printf ("\nStatus Kelulusan = LULUS");
+return 0;
+	}
